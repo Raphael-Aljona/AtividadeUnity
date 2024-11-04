9ddcebd21af7a795e500b8e327bdad5bb2c72f85
RAPHAEL ALVES ALJONA <53789144851@info.local>
2024-11-04 17:05:47 -0300

    feat: Exercicio 07

---
+++ Assets/Scripts/Exercicio 07.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using static Unity.Burst.Intrinsics.X86;
+using static UnityEditor.PlayerSettings;
+using static UnityEngine.EventSystems.EventTrigger;
+
+public class Exercicio07 : MonoBehaviour
+{
+
+    //    (Dado com N faces) Para fazer um jogo de RPG, vamos precisar
+    //  calcular probabilidades e simular a rolagem de dados.Crie um
+    //  script em que o usuário defina o número de faces de um dado
+    //  (int) e calcule a rolagem de um dado com esse número de faces.
+    //  Use a função abaixo para o cálculo: Random.Range (valor_min,
+    //  valor_max); (Coloque o código dentro da função-evento Start).
+
+    [SerializeField] int faces;
+    int resultado;
+
+
+
+    void Start()
+    {
+       
+            resultado = Random.Range(1, faces);
+            print(resultado);
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
