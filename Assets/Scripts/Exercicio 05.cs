1f60c10f4cdcc84e9b793c93ebf2feb74ad5fd49
RAPHAEL ALVES ALJONA <53789144851@info.local>
2024-11-04 16:42:30 -0300

    feat: Exercicio 05

---
+++ Assets/Scripts/Exercicio 05.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using static Unity.Burst.Intrinsics.X86;
+
+public class Exercicio05 : MonoBehaviour
+{
+    //(Pontuação de missão) Após completar uma missão, o jogador
+    //recebe pontos.Verifique se ele ganhou mais de 50 pontos.Se sim,
+    //exiba "Missão bem-sucedida"; caso contrário, "Missão
+    //incompleta".
+
+    string player;
+    [SerializeField] int pontos;
+
+    void Start()
+    {
+        if (pontos > 50)
+        {
+            print("Missão bem sucedida");
+        }else
+        {
+            print("Missão incompleta");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
