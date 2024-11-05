a6f7b5f21335a2cdeabcabe0a93d5d2f96905746
RAPHAEL ALVES ALJONA <53789144851@info.local>
2024-11-05 15:25:38 -0300

    feat: Exercicio 10

---
+++ Assets/Scripts/Exercicio 10.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class Exercicio10 : MonoBehaviour
+{
+    //    (Itens do inventário) Crie um script onde o jogador pode coletar
+    //  diferentes tipos de itens(como moedas, poções, ou power-ups)
+    //  que têm efeitos diferentes dependendo do tipo coletado.Use um
+    //  switch case para determinar o efeito de cada tipo de item.
+
+    int inventario;
+    bool moedas;
+    bool  powerUp;
+    bool pocoes;
+
+    void Start()
+    {
+        switch (inventario)
+        {
+            case 01:
+                if (moedas == true)
+                {
+                print("Você ficou muito rico $$");
+                }
+                break;
+
+            case 02:
+                if (powerUp == true)
+                {
+                    print("Sua força aumentou em 10x");
+                }
+                break;
+
+            case 03:
+                if (moedas == true)
+                {
+                    print("Você ganhou +1 vida!");
+                }
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
